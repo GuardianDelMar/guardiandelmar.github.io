7754827d53b647e1cb23dfa378a907d87156eca1
Patryk Gasek <pgasek2tm5@gmail.com>
2017-11-06 20:05:10 +0100

    literówki

--- zadania-domowe/pobierz_dane_programisty/js/main.js
+++ zadania-domowe/pobierz_dane_programisty/js/main.js
@@ -6,7 +6,7 @@ function createBox() {
 
 function Dane() {
     $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-        var programista = "<p>Imie:" + data.imie + " Nazwisko:" + data.nazwisko + " Zawod:" + data.zawod + " Firma:" + data.firma + "</p>";
+        var programista = "<p>Imię: " + data.imie + " Nazwisko: " + data.nazwisko + " Zawód: " + data.zawod + " Firma:" + data.firma + "</p>";
 
         if ($("#take_data").length === 0) {
             createBox();
